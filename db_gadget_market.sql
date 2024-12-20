22e33cc3d2c5bc3ccb49bec0f9328da790b20b33
nurynurul <y3301121@gmail.com>
2024-12-20 15:44:12 +0700

    perbaikan format database
    Meliputi
    1. Penghapusan tabel admin dan customer
    2. Pengahapusan atribut province pada tabel user
    3. Perbaikan struktur relasi database
    4. Perbaikan tipe data pada beberapa atribut tabel

--- db_gadget_market.sql
+++ db_gadget_market.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 19, 2024 at 12:13 AM
--- Server version: 10.4.32-MariaDB
--- PHP Version: 8.2.12
+-- Generation Time: Dec 20, 2024 at 09:32 AM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,24 +24,12 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `admin`
---
-
-CREATE TABLE `admin` (
-  `email` varchar(100) NOT NULL,
-  `password` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `categories`
 --
 
 CREATE TABLE `categories` (
-  `category_id` bigint(20) NOT NULL,
-  `name` varchar(255) NOT NULL,
-  `slug` varchar(255) NOT NULL
+  `category_id` int(1) NOT NULL,
+  `name` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -51,19 +39,9 @@ CREATE TABLE `categories` (
 --
 
 CREATE TABLE `city` (
-  `city_id` int(11) NOT NULL,
+  `city_id` int(1) NOT NULL,
   `city_name` varchar(100) NOT NULL,
-  `province_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `customer`
---
-
-CREATE TABLE `customer` (
-  `user_id` int(11) NOT NULL
+  `province_id` int(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -73,9 +51,9 @@ CREATE TABLE `customer` (
 --
 
 CREATE TABLE `favorites` (
-  `favorite_id` bigint(20) NOT NULL,
-  `user_id` int(11) NOT NULL,
-  `product_id` bigint(20) NOT NULL
+  `favorite_id` int(1) NOT NULL,
+  `user_id` int(1) NOT NULL,
+  `product_id` int(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -85,8 +63,8 @@ CREATE TABLE `favorites` (
 --
 
 CREATE TABLE `order` (
-  `order_id` bigint(20) NOT NULL,
-  `user_id` bigint(20) NOT NULL,
+  `order_id` int(1) NOT NULL,
+  `user_id` int(1) NOT NULL,
   `order_date` datetime NOT NULL,
   `payment_due` datetime NOT NULL,
   `payment_token` varchar(255) NOT NULL,
@@ -109,36 +87,14 @@ CREATE TABLE `order` (
   `customer_email` varchar(255) NOT NULL,
   `customer_city_id` varchar(255) NOT NULL,
   `customer_province_id` varchar(255) NOT NULL,
-  `customer_postcode` int(11) NOT NULL,
+  `customer_postcode` int(1) NOT NULL,
   `shipping_courier` varchar(255) NOT NULL,
   `shipping_service_name` varchar(255) NOT NULL,
-  `approved_by` bigint(20) NOT NULL,
+  `approved_by` int(1) NOT NULL,
   `approved_at` datetime NOT NULL,
   `cancelled_at` datetime NOT NULL,
-  `cancelled_by` bigint(20) NOT NULL,
+  `cancelled_by` int(1) NOT NULL,
   `cancellation_note` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `order_items`
---
-
-CREATE TABLE `order_items` (
-  `order_items_id` bigint(20) NOT NULL,
-  `qty` int(11) NOT NULL,
-  `base_price` int(11) NOT NULL,
-  `base_total` int(11) NOT NULL,
-  `tax_amount` decimal(16,0) NOT NULL,
-  `tax_percent` decimal(16,0) NOT NULL,
-  `discount_amount` decimal(16,0) NOT NULL,
-  `discount_percent` decimal(16,0) NOT NULL,
-  `sub_total` decimal(16,0) NOT NULL,
-  `name` varchar(255) NOT NULL,
-  `weight` varchar(255) NOT NULL,
-  `order_id` bigint(20) NOT NULL,
-  `product_id` bigint(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -148,16 +104,16 @@ CREATE TABLE `order_items` (
 --
 
 CREATE TABLE `product` (
-  `product_id` bigint(20) NOT NULL,
-  `quantity` int(11) NOT NULL,
-  `weight` int(11) NOT NULL,
+  `product_id` int(1) NOT NULL,
+  `quantity` int(1) NOT NULL,
+  `weight` int(1) NOT NULL,
   `description` text NOT NULL,
-  `category_id` bigint(20) NOT NULL,
-  `price` int(11) NOT NULL,
+  `category_id` int(1) NOT NULL,
+  `price` int(1) NOT NULL,
   `details` longtext NOT NULL,
-  `name` varchar(255) NOT NULL,
+  `name` varchar(100) NOT NULL,
   `status` tinyint(1) NOT NULL,
-  `review_able` tinyint(3) NOT NULL
+  `review_able` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -167,7 +123,7 @@ CREATE TABLE `product` (
 --
 
 CREATE TABLE `provinces` (
-  `province_id` int(11) NOT NULL,
+  `province_id` int(1) NOT NULL,
   `province_name` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -178,29 +134,22 @@ CREATE TABLE `provinces` (
 --
 
 CREATE TABLE `users` (
-  `user_id` int(11) NOT NULL,
+  `user_id` int(1) NOT NULL,
   `username` varchar(50) NOT NULL,
-  `first_name` int(50) NOT NULL,
+  `first_name` varchar(50) NOT NULL,
   `last_name` varchar(50) NOT NULL,
   `email` varchar(100) NOT NULL,
   `address` varchar(255) NOT NULL,
-  `phone` varchar(30) NOT NULL,
-  `city_id` int(11) NOT NULL,
-  `province_id` int(11) NOT NULL,
-  `postcode` int(11) NOT NULL,
-  `password` varchar(255) NOT NULL,
+  `phone` varchar(15) NOT NULL,
+  `city_id` int(1) NOT NULL,
+  `postcode` int(1) NOT NULL,
+  `password` varchar(50) NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Indexes for dumped tables
 --
-
---
--- Indexes for table `admin`
---
-ALTER TABLE `admin`
-  ADD PRIMARY KEY (`email`);
 
 --
 -- Indexes for table `categories`
@@ -216,12 +165,6 @@ ALTER TABLE `city`
   ADD KEY `province_id` (`province_id`);
 
 --
--- Indexes for table `customer`
---
-ALTER TABLE `customer`
-  ADD PRIMARY KEY (`user_id`);
-
---
 -- Indexes for table `favorites`
 --
 ALTER TABLE `favorites`
@@ -234,15 +177,7 @@ ALTER TABLE `favorites`
 --
 ALTER TABLE `order`
   ADD PRIMARY KEY (`order_id`),
-  ADD KEY `user_id` (`user_id`,`order_date`,`approved_by`,`cancelled_by`);
-
---
--- Indexes for table `order_items`
---
-ALTER TABLE `order_items`
-  ADD PRIMARY KEY (`order_items_id`),
-  ADD KEY `order_id` (`order_id`),
-  ADD KEY `name` (`name`);
+  ADD KEY `user_id` (`user_id`);
 
 --
 -- Indexes for table `product`
@@ -264,7 +199,6 @@ ALTER TABLE `users`
   ADD PRIMARY KEY (`user_id`),
   ADD UNIQUE KEY `username` (`username`),
   ADD UNIQUE KEY `email` (`email`),
-  ADD KEY `province_id` (`province_id`),
   ADD KEY `fk_city_id` (`city_id`);
 
 --
@@ -275,43 +209,37 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `categories`
 --
 ALTER TABLE `categories`
-  MODIFY `category_id` bigint(20) NOT NULL AUTO_INCREMENT;
+  MODIFY `category_id` int(1) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `city`
 --
 ALTER TABLE `city`
-  MODIFY `city_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `city_id` int(1) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `favorites`
 --
 ALTER TABLE `favorites`
-  MODIFY `favorite_id` bigint(20) NOT NULL AUTO_INCREMENT;
+  MODIFY `favorite_id` int(1) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `order`
 --
 ALTER TABLE `order`
-  MODIFY `order_id` bigint(20) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `order_items`
---
-ALTER TABLE `order_items`
-  MODIFY `order_items_id` bigint(20) NOT NULL AUTO_INCREMENT;
+  MODIFY `order_id` int(1) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `product`
 --
 ALTER TABLE `product`
-  MODIFY `product_id` bigint(20) NOT NULL AUTO_INCREMENT;
+  MODIFY `product_id` int(1) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `provinces`
 --
 ALTER TABLE `provinces`
-  MODIFY `province_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `province_id` int(1) NOT NULL AUTO_INCREMENT;
 
 --
 -- Constraints for dumped tables
@@ -321,33 +249,32 @@ ALTER TABLE `provinces`
 -- Constraints for table `city`
 --
 ALTER TABLE `city`
-  ADD CONSTRAINT `city_ibfk_1` FOREIGN KEY (`province_id`) REFERENCES `provinces` (`province_id`);
-
---
--- Constraints for table `customer`
---
-ALTER TABLE `customer`
-  ADD CONSTRAINT `customer_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE;
+  ADD CONSTRAINT `city_ibfk_1` FOREIGN KEY (`province_id`) REFERENCES `provinces` (`province_id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `favorites`
 --
 ALTER TABLE `favorites`
   ADD CONSTRAINT `favorites_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`),
-  ADD CONSTRAINT `favorites_ibfk_2` FOREIGN KEY (`product_id`) REFERENCES `product` (`product_id`);
+  ADD CONSTRAINT `favorites_ibfk_2` FOREIGN KEY (`product_id`) REFERENCES `product` (`product_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `order`
+--
+ALTER TABLE `order`
+  ADD CONSTRAINT `order_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `product`
 --
 ALTER TABLE `product`
-  ADD CONSTRAINT `product_ibfk_1` FOREIGN KEY (`category_id`) REFERENCES `categories` (`category_id`);
+  ADD CONSTRAINT `product_ibfk_1` FOREIGN KEY (`category_id`) REFERENCES `categories` (`category_id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `users`
 --
 ALTER TABLE `users`
-  ADD CONSTRAINT `fk_city_id` FOREIGN KEY (`city_id`) REFERENCES `city` (`city_id`) ON UPDATE CASCADE,
-  ADD CONSTRAINT `province_id` FOREIGN KEY (`province_id`) REFERENCES `provinces` (`province_id`) ON UPDATE CASCADE;
+  ADD CONSTRAINT `fk_city_id` FOREIGN KEY (`city_id`) REFERENCES `city` (`city_id`) ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;

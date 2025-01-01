939247e0a35a4e47eaaba9e48b32c2aff884613e
Adila Zahra <adilazsandrina@gmail.com>
2025-01-02 06:01:32 +0700

    Penambahan project berbasis Spring Boot

---
+++ IndiCellSpring/IndiCell/src/main/java/com/IPLKelompok/IndiCell/IndiCellApplication.java
@@ -0,0 +1,13 @@
+package com.IPLKelompok.IndiCell;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IndiCellApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IndiCellApplication.class, args);
+	}
+
+}
---
+++ IndiCellSpring/IndiCell/src/test/java/com/IPLKelompok/IndiCell/IndiCellApplicationTests.java
@@ -0,0 +1,13 @@
+package com.IPLKelompok.IndiCell;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IndiCellApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

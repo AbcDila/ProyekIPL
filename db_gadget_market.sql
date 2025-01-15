5c1ca9566feec5092be42724ac1993461127f1ce
ANGGITA DWI PUTRI <anggitadwiputri8@gmail.com>
2025-01-15 18:26:42 +0700

    Menambahkan tabel payments

--- db_gadget_market.sql
+++ db_gadget_market.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 21, 2024 at 05:09 PM
--- Server version: 10.4.32-MariaDB
--- PHP Version: 8.2.12
+-- Generation Time: Jan 15, 2025 at 12:24 PM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -26,319 +26,80 @@ SET time_zone = "+00:00";
 --
 -- Table structure for table `categories`
 --
-
-CREATE TABLE `categories` (
-  `category_id` int(1) NOT NULL,
-  `name` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+-- Error reading structure for table db_gadget_market.categories: #1932 - Table &#039;db_gadget_market.categories&#039; doesn&#039;t exist in engine
+-- Error reading data for table db_gadget_market.categories: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `db_gadget_market`.`categories`&#039; at line 1
 
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `city`
 --
-
-CREATE TABLE `city` (
-  `city_id` int(1) NOT NULL,
-  `city_name` varchar(100) NOT NULL,
-  `province_id` int(1) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+-- Error reading structure for table db_gadget_market.city: #1932 - Table &#039;db_gadget_market.city&#039; doesn&#039;t exist in engine
+-- Error reading data for table db_gadget_market.city: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `db_gadget_market`.`city`&#039; at line 1
 
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `favorites`
 --
-
-CREATE TABLE `favorites` (
-  `favorite_id` int(1) NOT NULL,
-  `user_id` int(1) NOT NULL,
-  `product_id` int(1) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+-- Error reading structure for table db_gadget_market.favorites: #1932 - Table &#039;db_gadget_market.favorites&#039; doesn&#039;t exist in engine
+-- Error reading data for table db_gadget_market.favorites: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `db_gadget_market`.`favorites`&#039; at line 1
 
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `order`
 --
-
-CREATE TABLE `order` (
-  `order_id` int(1) NOT NULL,
-  `user_id` int(1) NOT NULL,
-  `order_date` datetime NOT NULL,
-  `payment_due` datetime NOT NULL,
-  `payment_token` varchar(255) NOT NULL,
-  `payment_url` varchar(255) NOT NULL,
-  `payment_status` varchar(255) NOT NULL,
-  `status` varchar(255) NOT NULL,
-  `base_total_price` decimal(16,2) NOT NULL,
-  `tax_amount` decimal(16,2) NOT NULL,
-  `tax_percent` decimal(16,2) NOT NULL,
-  `discount_amount` decimal(16,2) NOT NULL,
-  `discount_percent` decimal(16,2) NOT NULL,
-  `shipping_cost` decimal(16,2) NOT NULL,
-  `grand_total` decimal(16,2) NOT NULL,
-  `note` text NOT NULL,
-  `customer_first_name` varchar(255) NOT NULL,
-  `customer_last_name` varchar(255) NOT NULL,
-  `customer_address1` varchar(255) NOT NULL,
-  `customer_address2` varchar(255) NOT NULL,
-  `customer_phone` varchar(255) NOT NULL,
-  `customer_email` varchar(255) NOT NULL,
-  `customer_city_id` varchar(255) NOT NULL,
-  `customer_province_id` varchar(255) NOT NULL,
-  `customer_postcode` int(1) NOT NULL,
-  `shipping_courier` varchar(255) NOT NULL,
-  `shipping_service_name` varchar(255) NOT NULL,
-  `approved_by` int(1) NOT NULL,
-  `approved_at` datetime NOT NULL,
-  `cancelled_at` datetime NOT NULL,
-  `cancelled_by` int(1) NOT NULL,
-  `cancellation_note` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+-- Error reading structure for table db_gadget_market.order: #1932 - Table &#039;db_gadget_market.order&#039; doesn&#039;t exist in engine
+-- Error reading data for table db_gadget_market.order: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `db_gadget_market`.`order`&#039; at line 1
 
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `order_items`
 --
+-- Error reading structure for table db_gadget_market.order_items: #1932 - Table &#039;db_gadget_market.order_items&#039; doesn&#039;t exist in engine
+-- Error reading data for table db_gadget_market.order_items: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `db_gadget_market`.`order_items`&#039; at line 1
 
-CREATE TABLE `order_items` (
-  `order_id` int(1) NOT NULL,
-  `product_id` int(1) NOT NULL,
-  `quantity` tinyint(4) DEFAULT NULL,
-  `weight` smallint(6) DEFAULT NULL,
-  `discount_percent` tinyint(4) DEFAULT NULL,
-  `discount_amount` decimal(16,2) DEFAULT NULL,
-  `base_price` decimal(16,2) DEFAULT NULL,
-  `base_total` decimal(16,2) DEFAULT NULL,
-  `tax_percent` tinyint(4) DEFAULT NULL,
-  `tax_amount` decimal(16,2) DEFAULT NULL,
-  `subtotal` decimal(16,2) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `payments`
+--
+-- Error reading structure for table db_gadget_market.payments: #1932 - Table &#039;db_gadget_market.payments&#039; doesn&#039;t exist in engine
+-- Error reading data for table db_gadget_market.payments: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `db_gadget_market`.`payments`&#039; at line 1
 
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `product`
 --
-
-CREATE TABLE `product` (
-  `product_id` int(1) NOT NULL,
-  `quantity` int(1) NOT NULL,
-  `weight` int(1) NOT NULL,
-  `description` text NOT NULL,
-  `category_id` int(1) NOT NULL,
-  `price` int(1) NOT NULL,
-  `details` longtext NOT NULL,
-  `name` varchar(100) NOT NULL,
-  `status` tinyint(1) NOT NULL,
-  `review_able` tinyint(1) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+-- Error reading structure for table db_gadget_market.product: #1932 - Table &#039;db_gadget_market.product&#039; doesn&#039;t exist in engine
+-- Error reading data for table db_gadget_market.product: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `db_gadget_market`.`product`&#039; at line 1
 
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `provinces`
 --
-
-CREATE TABLE `provinces` (
-  `province_id` int(1) NOT NULL,
-  `province_name` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+-- Error reading structure for table db_gadget_market.provinces: #1932 - Table &#039;db_gadget_market.provinces&#039; doesn&#039;t exist in engine
+-- Error reading data for table db_gadget_market.provinces: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `db_gadget_market`.`provinces`&#039; at line 1
 
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `review`
 --
-
-CREATE TABLE `review` (
-  `order_id` int(1) NOT NULL,
-  `user_id` int(1) NOT NULL,
-  `product_id` int(1) NOT NULL,
-  `rating` tinyint(4) NOT NULL,
-  `content` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+-- Error reading structure for table db_gadget_market.review: #1932 - Table &#039;db_gadget_market.review&#039; doesn&#039;t exist in engine
+-- Error reading data for table db_gadget_market.review: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `db_gadget_market`.`review`&#039; at line 1
 
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `users`
 --
-
-CREATE TABLE `users` (
-  `user_id` int(1) NOT NULL,
-  `username` varchar(50) NOT NULL,
-  `first_name` varchar(50) NOT NULL,
-  `last_name` varchar(50) NOT NULL,
-  `email` varchar(100) NOT NULL,
-  `address` varchar(255) NOT NULL,
-  `phone` varchar(15) NOT NULL,
-  `city_id` int(1) NOT NULL,
-  `postcode` int(1) NOT NULL,
-  `password` varchar(50) NOT NULL,
-  `created_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `categories`
---
-ALTER TABLE `categories`
-  ADD PRIMARY KEY (`category_id`);
-
---
--- Indexes for table `city`
---
-ALTER TABLE `city`
-  ADD PRIMARY KEY (`city_id`),
-  ADD KEY `province_id` (`province_id`);
-
---
--- Indexes for table `favorites`
---
-ALTER TABLE `favorites`
-  ADD PRIMARY KEY (`favorite_id`),
-  ADD KEY `user_id` (`user_id`),
-  ADD KEY `product_id` (`product_id`);
-
---
--- Indexes for table `order`
---
-ALTER TABLE `order`
-  ADD PRIMARY KEY (`order_id`),
-  ADD KEY `user_id` (`user_id`);
-
---
--- Indexes for table `order_items`
---
-ALTER TABLE `order_items`
-  ADD KEY `order_id` (`order_id`,`product_id`),
-  ADD KEY `product_id` (`product_id`);
-
---
--- Indexes for table `product`
---
-ALTER TABLE `product`
-  ADD PRIMARY KEY (`product_id`),
-  ADD KEY `category_id` (`category_id`);
-
---
--- Indexes for table `provinces`
---
-ALTER TABLE `provinces`
-  ADD PRIMARY KEY (`province_id`);
-
---
--- Indexes for table `review`
---
-ALTER TABLE `review`
-  ADD KEY `order_id` (`order_id`,`user_id`,`product_id`),
-  ADD KEY `user_id` (`user_id`),
-  ADD KEY `product_id` (`product_id`);
-
---
--- Indexes for table `users`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`user_id`),
-  ADD UNIQUE KEY `username` (`username`),
-  ADD UNIQUE KEY `email` (`email`),
-  ADD KEY `fk_city_id` (`city_id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `categories`
---
-ALTER TABLE `categories`
-  MODIFY `category_id` int(1) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `city`
---
-ALTER TABLE `city`
-  MODIFY `city_id` int(1) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `favorites`
---
-ALTER TABLE `favorites`
-  MODIFY `favorite_id` int(1) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `order`
---
-ALTER TABLE `order`
-  MODIFY `order_id` int(1) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `product`
---
-ALTER TABLE `product`
-  MODIFY `product_id` int(1) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `provinces`
---
-ALTER TABLE `provinces`
-  MODIFY `province_id` int(1) NOT NULL AUTO_INCREMENT;
-
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `city`
---
-ALTER TABLE `city`
-  ADD CONSTRAINT `city_ibfk_1` FOREIGN KEY (`province_id`) REFERENCES `provinces` (`province_id`) ON DELETE CASCADE ON UPDATE CASCADE;
-
---
--- Constraints for table `favorites`
---
-ALTER TABLE `favorites`
-  ADD CONSTRAINT `favorites_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`),
-  ADD CONSTRAINT `favorites_ibfk_2` FOREIGN KEY (`product_id`) REFERENCES `product` (`product_id`) ON DELETE CASCADE ON UPDATE CASCADE;
-
---
--- Constraints for table `order`
---
-ALTER TABLE `order`
-  ADD CONSTRAINT `order_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE;
-
---
--- Constraints for table `order_items`
---
-ALTER TABLE `order_items`
-  ADD CONSTRAINT `order_items_ibfk_1` FOREIGN KEY (`order_id`) REFERENCES `order` (`order_id`) ON DELETE NO ACTION ON UPDATE CASCADE,
-  ADD CONSTRAINT `order_items_ibfk_2` FOREIGN KEY (`product_id`) REFERENCES `product` (`product_id`) ON DELETE NO ACTION ON UPDATE CASCADE;
-
---
--- Constraints for table `product`
---
-ALTER TABLE `product`
-  ADD CONSTRAINT `product_ibfk_1` FOREIGN KEY (`category_id`) REFERENCES `categories` (`category_id`) ON DELETE CASCADE ON UPDATE CASCADE;
-
---
--- Constraints for table `review`
---
-ALTER TABLE `review`
-  ADD CONSTRAINT `review_ibfk_1` FOREIGN KEY (`order_id`) REFERENCES `order` (`order_id`) ON DELETE NO ACTION ON UPDATE CASCADE,
-  ADD CONSTRAINT `review_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE NO ACTION ON UPDATE CASCADE,
-  ADD CONSTRAINT `review_ibfk_3` FOREIGN KEY (`product_id`) REFERENCES `product` (`product_id`) ON DELETE CASCADE ON UPDATE CASCADE;
-
---
--- Constraints for table `users`
---
-ALTER TABLE `users`
-  ADD CONSTRAINT `fk_city_id` FOREIGN KEY (`city_id`) REFERENCES `city` (`city_id`) ON UPDATE CASCADE;
+-- Error reading structure for table db_gadget_market.users: #1932 - Table &#039;db_gadget_market.users&#039; doesn&#039;t exist in engine
+-- Error reading data for table db_gadget_market.users: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `db_gadget_market`.`users`&#039; at line 1
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
